4b8a84061d8e940cc6cb3991bc1fafb2b70c43ad
SanketTambe39 <sankettambe39@gmail.com>
2021-06-22 20:15:57 +0530

    Created : Master branch

---
+++ SnackAndLadderSimulator/src/com/SnackAndLadderSimulator/SnackAndLadderSimulator.java
@@ -0,0 +1,11 @@
+package com.SnackAndLadderSimulator;
+
+public class SnackAndLadderSimulator {
+
+	public static void main(String[] args) {
+		
+		System.out.println("Welcome to Snack And Ladder Simulator Program.");
+
+	}
+
+}
